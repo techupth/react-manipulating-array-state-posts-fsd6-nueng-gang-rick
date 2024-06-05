df3e714eb1aeeca639eba1728ee2db015c7598ec
nueng <wongsaokrn.kump@gmail.com>
2024-06-05 12:41:07 +0700

    feat: add eventListner onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import { blog } from "./components/blogpost";
 import Posts from "./components/Posts";
 
 function App() {
   return (
     <div className="App">
-      <Posts />
+      {blog.map((item)=>{
+        return(<Posts 
+        key={item.id}
+        title={item.title}
+        like={item.likes}
+        content={item.content} />)
+      })}
     </div>
   );
 }
